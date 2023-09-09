d773b0d7c6ef0c59ac6901b62358068009e9a598
orkeilius <orkeilius@users.noreply.github.com>
2023-09-09 13:01:20 +0200

    add better response to login

--- back-end/src/main/java/com/discrotte/backend/Controller.java
+++ back-end/src/main/java/com/discrotte/backend/Controller.java
@@ -2,19 +2,22 @@ package com.discrotte.backend;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class Controller {
 
 	@PostMapping("/login")
 	public String Login(@RequestParam String name,@RequestParam String password) {
-		System.out.println(User.QueryUser(name).name);
-		if (User.QueryUser(name).CheckPassword(password)) {
-			return "connected";
+		User user = User.QueryUser(name);
+		if (user.CheckPassword(password)) {
+			return user.name;
 		}
-		return "invalid ";
+		throw new ResponseStatusException(
+		           HttpStatus.FORBIDDEN);
 	}
 }
