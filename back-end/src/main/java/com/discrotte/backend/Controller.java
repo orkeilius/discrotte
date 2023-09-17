bae8a379acdd2fd6ff8b36eaf826704e5dd7be69
orkeilius <orkeilius@users.noreply.github.com>
2023-09-17 19:55:57 +0200

    add working cors

--- back-end/src/main/java/com/discrotte/backend/Controller.java
+++ back-end/src/main/java/com/discrotte/backend/Controller.java
@@ -1,7 +1,9 @@
 package com.discrotte.backend;
 
+import java.util.HashMap;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,21 +14,33 @@ import com.discrotte.backend.model.User;
 import com.discrotte.backend.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173",allowCredentials = "true")
 public class Controller {
 	
 	 @Autowired
 	 private UserService userService;
+	 
+	 @PostMapping("/getUser")
+	 public HashMap<String, String> getUser(@RequestParam String username) {
+		 Optional<User> request = userService.getUser(username);
+		 if (request.isEmpty()) {
+			 throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Unknown user");
+		 }
+		 HashMap<String, String> user = new HashMap<>();
+		 
+		 user.put("name", request.get().name);
+		 user.put("role", "USER");
+		 	
+		 return user;
+		}
 	
-	
-
 	@PostMapping("/createUSer")
 	public void CreateUser(@RequestParam String name,@RequestParam String password) {
 		userService.saveUser(new User(name, password));
 	}
-	@PostMapping("/ping")
-	public String Logout() {
-		userService.saveUser(new User("e","e"));
-		return "pong";
+	@PostMapping("/login")
+	public Void Login() {
+		return null;
 		//placeholder for logout
 	}
 	@PostMapping("/message")
--- back-end/src/main/java/com/discrotte/backend/security/WebSecurityConfig.java
+++ back-end/src/main/java/com/discrotte/backend/security/WebSecurityConfig.java
@@ -4,16 +4,23 @@ package com.discrotte.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Arrays;
+@EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
 	
@@ -23,21 +30,30 @@ public class WebSecurityConfig {
         http
             .authorizeHttpRequests((authz) -> authz
             	//.requestMatchers("/h2-console/**").permitAll()
+            	.requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
             	.requestMatchers(new AntPathRequestMatcher("/ping")).permitAll()
             	.requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
-            	.requestMatchers(new AntPathRequestMatcher("/message")).authenticated()
+            	.anyRequest().authenticated()
             	
             );
             //.formLogin(withDefaults())  
         http.httpBasic(withDefaults());
-        http
-		// ...
-		.securityContext((securityContext) -> securityContext
+		http.securityContext((securityContext) -> securityContext
 			.securityContextRepository(new RequestAttributeSecurityContextRepository())
 		);
         return http.build();
     }
    
+ 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173/","*","file://**","file:///C:/Users/orkeilius/Desktop/code/discrotte/doc/test.html"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
     
     @Bean
     public UserDetailsService userDetailsService() {
