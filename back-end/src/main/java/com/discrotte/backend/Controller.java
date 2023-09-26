77c564f3d0d78daeaacb26e346ff3285f79eb418
orkeilius <orkeilius@users.noreply.github.com>
2023-09-26 15:50:39 +0200

    fix bug in message/send

--- back-end/src/main/java/com/discrotte/backend/Controller.java
+++ back-end/src/main/java/com/discrotte/backend/Controller.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -77,11 +79,8 @@ public class Controller {
 
 	@PostMapping("/message/send")
 	public void sendMessage(@RequestBody String text) {
-		Optional<User> author = userService.getCurrentUser();
-		if (author.isEmpty()) {
-			throw new ResponseStatusException(HttpStatus.NOT_MODIFIED, "Unknown user");
-		}
-		messageService.saveMessage(new Message(author.get(), text));
+		User author = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		messageService.saveMessage(new Message(author, text));
 	}
 
 	@PostMapping(path = "/message/getOld")
